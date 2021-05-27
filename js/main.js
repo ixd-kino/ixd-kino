9a64ef1e87f1367bf6070850d862a774a4e2b2b4
ixd-kino <roncobilaccio97@gmail.com>
2021-05-27 13:46:39 +0200

    flexbox update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var toggleNav = function() {
+    $(".navbar").toggleClass("expanded");
+    $(".link-wrapper").toggleClass("open");
+    $(".navbar-nav").toggleClass("open");
+    $(".nav-wrapper").toggleClass("open");
+    $(".cta-wrapper").toggleClass("open");
+    $(".menu-icon").toggleClass("open");
+    $(".close-icon").toggleClass("open");
+    $(".c").toggleClass("hide");
+    $(".w").toggleClass("hide");
+}
